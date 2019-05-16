b44cdedfb8b35f1e1dbd01fe3dd7ed7995642d29
yItblack <1227107932@qq.com>
2019-05-17 00:24:50 +0800

    github commit

---
+++ src/main/java/com/example/spring03_config/Spring03ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.example.spring03_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring03ConfigApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("this is commit");
+		System.out.println("this onr commit");
+
+		SpringApplication.run(Spring03ConfigApplication.class, args);
+	}
+
+}
